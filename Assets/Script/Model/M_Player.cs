4b698647696fef95ccc92cb8c97936194977539b
woojin Jo <woojin.jo95@gmail.com>
2016-07-18 21:27:41 +0900

    update M_Player in diagram and unity.

--- Assets/Script/Model/M_Player.cs
+++ Assets/Script/Model/M_Player.cs
@@ -4,14 +4,23 @@ using System.Collections;
 public class M_Player : MonoBehaviour {
     int _Gold;
     int _Power;
+    int _lastStep;
 
-    void CalcGold(int step)
+    //Controller에서 부를 것이니...
+    void setLastStep(int givenStep)
     {
-
+        this._lastStep = givenStep;
     }
-    void CalcPower(int step)
+
+    //골드 계산방법 
+    int CalcGold()
     {
-
+        return 0;
     }
-    
+
+    //전력 계산방법 : 현재 전력 + (현재 걸음 수 - 마지막 걸음 수)
+    int CalcPower(int currentStep)
+    {
+        return this._Power + (currentStep - this._lastStep);
+    }
 }
