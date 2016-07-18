af4d7c44a2bb933f1b28cdae35e50c85d4ad0772
sentendro <ghu333@hanmail.net>
2016-07-18 16:19:18 +0900

    Start

---
+++ Assets/Script/C_SceneController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class C_SceneController : MonoBehaviour {
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void SceneMove(string name)
+    {
+        switch (name)
+        {
+            //Logo Scene
+            case "Start":   //냉장고 씬으로 넘어 간다.
+                SceneManager.LoadScene("01_Refrig");
+                break;
+            case "End":     //앱을 종료 시킨다.
+                Application.Quit();               
+                break;
+
+            //Refrig Scene
+            case "MyPage":
+                SceneManager.LoadScene("02_Mypage");
+                break;
+            case "Shop":
+                SceneManager.LoadScene("02_Shop");
+                break;
+            case "Setting":
+                SceneManager.LoadScene("02_Setting");
+                break;
+
+            //MyPage Scene
+            case "MyPage_Back":
+                SceneManager.LoadScene("01_Refrig");
+                break;
+            //Shop Scene
+            case "Shop_Back":
+                SceneManager.LoadScene("01_Refrig");
+                break;
+            //Setting Scene
+            case "Setting_Back":
+                SceneManager.LoadScene("01_Refrig");
+                break;
+
+
+
+
+
+
+            //default
+            default:
+                print("");
+                break;
+
+        }
+            
+
+    }
+
+}
---
+++ Assets/Script/V_EventHandler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class V_EventHandler : MonoBehaviour {
+    public GameObject SceneController;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            string SelectedButton = EventSystem.current.currentSelectedGameObject.name;
+
+            //빈곳을 클릭한게 아닐 때
+            if(!SelectedButton.Equals(null))
+                SceneController.SendMessage("SceneMove", SelectedButton);
+        }
+    }
+}
