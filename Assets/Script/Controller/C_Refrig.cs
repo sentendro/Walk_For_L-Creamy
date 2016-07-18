b34d7a50dc2c2e221ad675c8ad19f83db09dbdac
woojin Jo <woojin.jo95@gmail.com>
2016-07-18 22:42:15 +0900

    add Controller Object and Scripts in all Scenes.

---
+++ Assets/Script/Controller/C_Refrig.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class C_Refrig : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
