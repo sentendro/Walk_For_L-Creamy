400a3ae3f5ebdd6cb6ca4930852ea4884b96f272
sentendro <ghu333@hanmail.net>
2016-07-24 15:45:49 +0900

    implement Serialzer

--- Assets/Script/Controller/C_Serializer.cs
+++ Assets/Script/Controller/C_Serializer.cs
@@ -1,15 +1,41 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System;
 
-public class C_Serializer : MonoBehaviour {
+public class C_Serializer{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    #region serialize
+    public static string ObjectToString(Object obj)
+    {
+        using (var memoryStream = new MemoryStream())
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(memoryStream, obj);
+            memoryStream.Flush();
+            memoryStream.Position = 0;
+
+            return Convert.ToBase64String(memoryStream.ToArray());
+        }
+    }
+    #endregion
+
+    #region Deserialize
+    public static T Deserialize<T>(string xmlText)
+    {
+        if (xmlText != null && xmlText != String.Empty)
+        {
+            byte[] b = Convert.FromBase64String(xmlText);
+            using (var stream = new MemoryStream(b))
+            {
+                var formatter = new BinaryFormatter();
+                stream.Seek(0, SeekOrigin.Begin);
+                return (T)formatter.Deserialize(stream);
+            }
+        }
+        else
+        {
+            return default(T);
+        }
+    }
+    #endregion
 }
