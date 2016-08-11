f1f5c0e12a9c360173d4808e1ddf8557c5a22a0e
sentendro <ghu333@hanmail.net>
2016-08-11 15:28:07 +0900

    add stepDetector on walker

--- Assets/Script/Model/M_Walker.cs
+++ Assets/Script/Model/M_Walker.cs
@@ -7,6 +7,13 @@ public class M_Walker : MonoBehaviour
     private float _step;
     private float _time;
     private GameObject C_Refrig;
+    private float _loLim = 0.005F;
+    private float _hiLim = 0.3F;
+    private bool stateH = false;
+    private float _fHigh = 8.0F;
+    private float _curAcc = 0F;
+    private float _fLow = 0.2F;
+    private float _avgAcc;
     float deltaTime;
     void Start()
     {
@@ -21,8 +28,6 @@ public class M_Walker : MonoBehaviour
         deltaTime += Time.deltaTime;
         this._time = (int)deltaTime;
         this._step += (Time.deltaTime)*4;
-
-        
     }
     void getStep(C_Refrig script)
     {
@@ -32,5 +37,27 @@ public class M_Walker : MonoBehaviour
     void getTime(C_Refrig script)
     {
         script._time = (int) this._time;
+    }
+
+    public void stepDetector()
+    {
+        _curAcc = Mathf.Lerp(_curAcc, Input.acceleration.magnitude, Time.deltaTime * _fHigh);
+        _avgAcc = Mathf.Lerp(_avgAcc, Input.acceleration.magnitude, Time.deltaTime * _fLow);
+        float delta = _curAcc - _avgAcc;
+        if (!stateH)
+        {
+            if (delta > _hiLim)
+            {
+                stateH = true;
+                this._step++;
+            }
+        }
+        else {
+            if (delta < _loLim)
+            {
+                stateH = false;
+            }
+        }
+        _avgAcc = _curAcc;
     }
 }
